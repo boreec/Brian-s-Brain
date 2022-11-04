3d9c6fc50fb4a87550b85ff5af089276916cb7cc
Cyprien Borée <boreec@tuta.io>
2022-11-04 19:55:34 +0900

    add doc to WorldState struct and WorldState::new()

--- src/world_state.rs
+++ src/world_state.rs
@@ -1,8 +1,13 @@
+
+/// This struct represents the entire Cellular Automaton. 
 pub struct WorldState {
     size: u16,
 }
 
 impl WorldState {
+    
+    /// Create a new **WorldState** with a defined size.
+    /// The size provided is used for the width and the height.
     pub fn new(w_size: u16) -> WorldState {
         WorldState {
             size: w_size,
