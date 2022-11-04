7d99e7cae22c783a4e0c98c175c84637ebcac264
Cyprien Borée <boreec@tuta.io>
2022-11-04 20:13:12 +0900

    add doc to Args::size

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod world_state;
 #[command(author, version, about, long_about = None)]
 struct Args {
     
+    /// The size of the world in which the cells live.    
     #[arg(short, long, default_value_t = 10)]
     size: u16,     
 }
