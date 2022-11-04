adffa14a972159efe20204bbef331c8fb434ab29
Cyprien Borée <boreec@tuta.io>
2022-11-04 23:56:50 +0900

    use field shorthand fort WorldState::size in WorldState::new()

--- src/world_state.rs
+++ src/world_state.rs
@@ -8,9 +8,9 @@ impl WorldState {
     
     /// Create a new **WorldState** with a defined size.
     /// The size provided is used for the width and the height.
-    pub fn new(w_size: u16) -> WorldState {
+    pub fn new(size: u16) -> WorldState {
         WorldState {
-            size: w_size,
+            size,
         }
     }
 }
