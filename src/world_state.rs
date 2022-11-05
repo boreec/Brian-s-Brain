392ca5f68aa2f965fdded9e28854393387e175ca
Cyprien Borée <boreec@tuta.io>
2022-11-05 17:29:31 +0900

    add doc to enum CellState

--- src/world_state.rs
+++ src/world_state.rs
@@ -1,5 +1,11 @@
 use array2d::Array2D;
 
+/// The three states a cell can take.
+/// Each cell is considered to have 8 neighbors (the Moore neighborhood).
+/// In each time step, a cell turns on if it was **Off** but had exactly two neighbors
+/// that were on. All cells that were **On** go into the **Dying** state, which is not
+/// counted as an **On** cell in the neighbor count, and prevents any cell from being
+/// born there. Cells that were in the **Dying** state go into the **Off** state. 
 #[derive(Clone)]
 enum CellState {
     On,
@@ -22,5 +28,5 @@ impl WorldState {
             size,
             world: Array2D::filled_with(CellState::Off, size as usize, size as usize),
         }
-    }
+    }    
 }
