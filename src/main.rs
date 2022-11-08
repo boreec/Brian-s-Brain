2b52faeb15bb14c787ce05d94c58321eb71a4058
Cyprien Borée <boreec@tuta.io>
2022-11-08 21:00:43 +0900

    add fragment shader

--- src/main.rs
+++ src/main.rs
@@ -215,5 +215,19 @@ fn init_vulkan() -> Result<(), Box<dyn Error>>{
             }"
         }
     }    
+    
+    mod fs {
+        vulkano_shaders::shader! {
+            ty: "fragment",
+            src:
+            "#version 450
+
+            layout(location = 0) out vec4 f_color;
+            
+            void main(){
+                f_color = vec4(1.0, 0.0, 0.0, 1.0);
+            }"
+        }
+    }
     Ok(())
 }
