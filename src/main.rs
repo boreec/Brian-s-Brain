11adb33b208a6c3c199662bdd50c20b1741cb413
Cyprien Borée <boreec@tuta.io>
2022-11-11 18:48:28 +0900

    add iter arg to set a number of iterations to run for

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ mod world_state;
 #[command(author, version, about, long_about = None)]
 struct Args {
     
+    /// The number of iterations to run for.
+    #[arg(short, long, default_value_t = 100)]
+    iter: u16,
+    
     /// The size of the world in which the cells live.    
     #[arg(short, long, default_value_t = 10)]
     size: u16,
