a5512667bc99e3c36af9480e9d4b384cb7d9581c
Cyprien Borée <boreec@tuta.io>
2022-11-19 19:49:00 +0900

    position window on the center of the monitor

--- src/graphics/window.rs
+++ src/graphics/window.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use std::error::Error;
 use std::sync::Arc;
 
 use vulkano::instance::Instance;
@@ -30,15 +31,29 @@ pub const WINDOW_INNER_SIZE: Size = Size::Physical(
 );
 
 pub fn create_surface(instance: &Arc<Instance>, event_loop: &EventLoop<()>)
--> Result<Arc<Surface>, CreationError> 
+-> Result<Arc<Surface>, Box<dyn Error>> 
 {
-    WindowBuilder::new()
+    
+    let monitor = select_best_monitor(event_loop)
+        .ok_or_else(|| Box::<dyn Error>::from("No monitors found for GUI."))?;
+    
+    let monitor_size = monitor.size();
+    let centered_h = monitor_size.height / 2 - WINDOW_HEIGHT / 2;
+    let centered_w = monitor_size.width / 2 - WINDOW_WIDTH / 2;
+
+    let centered_pos : Position = Position::Physical(
+        PhysicalPosition::new(centered_w as i32, centered_h as i32) 
+    );
+    
+    let window = WindowBuilder::new()
         .with_resizable(false)
         .with_min_inner_size(WINDOW_INNER_SIZE)
         .with_max_inner_size(WINDOW_INNER_SIZE)
         .with_title(String::from(WINDOW_TITLE))
-        .build_vk_surface(event_loop, instance.clone())
+        .with_position(centered_pos)
+        .build_vk_surface(event_loop, instance.clone())?;
     
+    Ok(window)
 }
 
 fn select_best_monitor(event_loop: &EventLoop<()>) -> Option<MonitorHandle> {
